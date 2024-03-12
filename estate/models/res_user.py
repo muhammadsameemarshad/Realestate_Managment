c9eeb5e7fb9701fb8cd5eab35bf36baa4a390432
muhammadsameemarshad <146926387+muhammadsameemarshad@users.noreply.github.com>
2024-03-12 10:01:43 +0500

    Update res_user.py

--- estate/models/res_user.py
+++ estate/models/res_user.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class ResUser(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.one2many('estate.property', inverse="salesman")
+    property_ids = fields.one2many('estate.property', inverse_name="salesman")
